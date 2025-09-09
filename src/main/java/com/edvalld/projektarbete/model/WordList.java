986c547b6f2194f07d091d80410d43d0aea219b1
EdvaLLd <63045045+EdvaLLd@users.noreply.github.com>
2025-09-09 13:43:52 +0200

    initial setup

---
+++ src/main/java/com/edvalld/projektarbete/model/WordList.java
@@ -0,0 +1,9 @@
+package com.edvalld.projektarbete.model;
+
+public class WordList {
+
+    String name;
+
+
+
+}
---
+++ src/test/java/com/edvalld/projektarbete/ProjektarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvalld.projektarbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektarbeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
